93f421a1303aa3d201b8bb8ae588b24edda2ebae
leejungwoo <ljw69700@gmail.com>
2020-04-11 19:34:07 +0900

    4. landmark teeth

--- generate_landmark_teeth.py
+++ generate_landmark_teeth.py
@@ -1,10 +1,8 @@
 import os
-import sys
-import math
+import glob
 import dlib
 import cv2
 import numpy as np
-import glob
 import argparse
 
 detector = dlib.get_frontal_face_detector()
@@ -40,24 +38,9 @@ def shape_to_np(shape, dtype="int"):
     # return the list of (x, y)-coordinates
     return coords
 
-def masked_histogram_equalization(img, mask):
-    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-    cnt=0
-    elems = []
-    for y in range(gray.shape[0]):
-        for x in range(gray.shape[1]):
-            if mask[y, x] != 0:
-                elems.insert(cnt, gray[y, x])
-                cnt += 1
-    hist, bins = np.histogram(elems, 256, [0, 256])
-    cdf = hist.cumsum()
-    cdf_m = np.ma.masked_equal(cdf,0)
-    cdf_m = (cdf_m - cdf_m.min())*255/(cdf_m.max()-cdf_m.min())
-    
-    cdf = np.ma.filled(cdf_m,0).astype('uint8')
-    return cdf[gray]
+err_list = []
 
-file_list = glob.glob(input_folder+'**/*.*', recursive=True)
+file_list = glob.glob(input_folder+'/**/*.*', recursive=True)
 for file in file_list:
     print("reading file: %s" % file)
     img = cv2.imread(file)
@@ -74,6 +57,7 @@ for file in file_list:
     rects = detector(adaptive, 1)
     if len(rects) == 0:
         print("error finding face at file: %s" % file)
+        err_list.insert(len(err_list), file)
         continue
     
     # 얼굴이 하나임을 가정
@@ -83,37 +67,6 @@ for file in file_list:
 
     # 0~16 : 턱(2~14:광대 아래쪽) / 31~35 : 아래코 / 48~67 : mouth
     # 얼굴 하관 점들의 좌표
-    # under_shape = np.concatenate((shape[2:15], np.flip(shape[31:36], axis=0)), axis=0)
-    # mouth_shape = shape[48:68]
-    # jaw = shape[0:17]
-    # left_eyebrow = shape[22:27]
-    # right_eyebrow = shape[17:22]
-    # nose_bridge = shape[27:31]
-    # lower_nose = shape[30:35]
-    # left_eye = shape[42:48]
-    # right_eye = shape[36:42]
-    # outer_lip = shape[48:60]
-    # inner_lip = shape[60:68]
-    # jaw = shape[0:17]
-    # left_eyebrow = shape[22:27]
-    # right_eyebrow = shape[17:22]
-    # nose_bridge = shape[27:31]
-    # lower_nose = shape[30:35]
-    # left_eye = shape[42:48]
-    # right_eye = shape[36:42]
-    # outer_lip = shape[48:60]
-    # inner_lip = shape[60:68]
-
-    # cv2.polylines(img, [jaw], False, (0, 0, 0), 1)
-    # cv2.polylines(img, [left_eyebrow], False, (0, 0, 0), 1)
-    # cv2.polylines(img, [right_eyebrow], False, (0, 0, 0), 1)
-    # cv2.polylines(img, [nose_bridge], False, (0, 0, 0), 1)
-    # cv2.polylines(img, [lower_nose], True, (0, 0, 0), 1)
-    # cv2.polylines(img, [left_eye], True, (0, 0, 0), 1)
-    # cv2.polylines(img, [right_eye], True, (0, 0, 0), 1)
-    # cv2.polylines(img, [outer_lip], True, (0, 0, 0), 1)
-    # cv2.polylines(img, [inner_lip], True, (0, 0, 0), 1)
-
     under_shape = shape[2:15]
     outer_lip = shape[48:60]
     inner_lip = shape[60:68]
@@ -123,68 +76,59 @@ for file in file_list:
     cv2.polylines(landmark, [under_shape], False, (255, 255, 255), 1)
     cv2.polylines(landmark, [outer_lip], True, (255, 255, 255), 1)
     cv2.polylines(landmark, [inner_lip], True, (255, 255, 255), 1)
-
-    # find canny
-    # (mx, my, mw, mh) = np.dot(3, get_rect(inner_lip))
-    # inner_lip_crop = img[my:my+mh, mx:mx+mw]
-    # mask = np.zeros((mh, mw), dtype=np.uint8)
-    # inner_lip_org = np.dot(3, inner_lip)
-    # for s in inner_lip_org:
-    #     s -= [mx, my]
-    # cv2.fillPoly(mask, [inner_lip_org], 255)
-    # # mask_erode = cv2.erode(mask, np.ones((5, 5), np.uint8), iterations=1)
-    # sharpening = np.array([[-1, -1, -1, -1, -1],
-    #                         [-1, 2, 2, 2, -1],
-    #                         [-1, 2, 9, 2, -1],
-    #                         [-1, 2, 2, 2, -1],
-    #                         [-1, -1, -1, -1, -1]]) / 9.0
-    # inner_lip_crop = cv2.filter2D(inner_lip_crop, -1, sharpening)
-    # inner_lip_masked = cv2.bitwise_and(inner_lip_crop, inner_lip_crop, mask=mask)
-
-    # # gray_imm = masked_histogram_equalization(inner_lip_masked, mask)
-
-    # # gray_imm = cv2.medianBlur(gray_imm, 3)
-
-    # # ret, gray_imm = cv2.threshold(gray_imm, 159, 255, cv2.THRESH_BINARY)
-    # # hori = np.array([[1, 1, 1, 1, 1], [1, 1, 1, 1, 1] , [1, 1, 1, 1, 1]])
-    # # gray_imm = cv2.erode(gray_imm, np.ones((5, 5), np.uint8), iterations=1)
-    # # gray_imm = cv2.dilate(gray_imm, np.ones((5, 5), np.uint8), iterations=1)
-    # # gray_imm = cv2.erode(gray_imm, np.ones((5, 5), np.uint8), iterations=1)
-    # # gray_imm = cv2.erode(gray_imm, hori, iterations=1)
-    # # gray_imm = cv2.dilate(gray_imm, hori, iterations=1)
-    # # gray_imm = cv2.erode(gray_imm, hori, iterations=2)
     
-    # tooth = cv2.Canny(inner_lip_masked, 100, 150)
+    # find canny
+    (mx, my, mw, mh) = get_rect(inner_lip)
+    inner_lip_crop = img[my:my+mh, mx:mx+mw]
+    mask = np.zeros((mh, mw), dtype=np.uint8)
+    inner_lip_s = np.array(inner_lip)
+    for s in inner_lip_s:
+        s -= [mx, my]
+    cv2.fillPoly(mask, [inner_lip_s], 255)
+    mask = cv2.erode(mask, np.ones((3, 3), np.uint8), iterations=1)
+    sharpening = np.array([[-1, -1, -1, -1, -1],
+                            [-1, 2, 2, 2, -1],
+                            [-1, 2, 9, 2, -1],
+                            [-1, 2, 2, 2, -1],
+                            [-1, -1, -1, -1, -1]]) / 9.0
+    inner_lip_crop = cv2.filter2D(inner_lip_crop, -1, sharpening)
+    inner_lip_masked = cv2.bitwise_and(inner_lip_crop, inner_lip_crop, mask=mask)
+    
+    tooth = cv2.Canny(inner_lip_masked, 100, 120)
+    tooth = cv2.bitwise_and(tooth, tooth, mask=mask)
 
-    # lines = cv2.HoughLines(tooth, 1, np.pi/2, 20)
+    lines = cv2.HoughLines(tooth, 1, np.pi/180, 10)
+    (x1, y1, x2, y2) = (0, 0, 0, 0)
+    tooth_landmark = np.zeros(img.shape[:2], dtype='uint8')
+    if lines is not None:
+        for i in range(len(lines)):
+            for rho, theta in lines[i]:
+                if theta > 1:
+                    a = np.cos(theta)
+                    b = np.sin(theta)
+                    x0 = a*rho
+                    y0 = b*rho
+                    x1 += int(x0 + 1000*(-b))
+                    x2 += int(x0 - 1000*(-b))
+                    y1 += int(y0+1000*(a))
+                    y2 += int(y0 -1000*(a))
+        x1 /=len(lines)
+        x2 /=len(lines)
+        y1 /=len(lines)
+        y2 /=len(lines)
+        cv2.line(tooth_landmark, (mx+int(x1), my+int(y1)),(mx+int(x2), my+int(y2)), 255, 1)
 
-    # (x1, y1, x2, y2) = (0, 0, 0, 0)
-    # tooth_landmark = np.zeros(img.shape[:2], dtype='uint8')
-    # if lines is not None:
-    #     for i in range(len(lines)):
-    #         for rho, theta in lines[i]:
-    #             if theta > 1:
-    #                 a = np.cos(theta)
-    #                 b = np.sin(theta)
-    #                 x0 = a*rho
-    #                 y0 = b*rho
-    #                 x1 += int(x0 + 1000*(-b))
-    #                 x2 += int(x0 - 1000*(-b))
-    #                 y1 += int(y0+1000*(a))
-    #                 y2 += int(y0 -1000*(a))
-    #     x1 /=len(lines)
-    #     x2 /=len(lines)
-    #     y1 /=len(lines)
-    #     y2 /=len(lines)
-    #     cv2.line(tooth_landmark, (mx+int(x1), my+int(y1)),(mx+int(x2), my+int(y2)), 255, 1)
-
-    # tooth_landmark = cv2.bitwise_and(tooth_landmark, tooth_landmark, mask=mask)
-    # tooth_landmark = cv2.resize(tooth_landmark, None, fx=ratio, fy=ratio)
-    # landmark += tooth_landmark
+    mask = np.zeros(landmark.shape[:2], dtype=np.uint8)
+    cv2.fillPoly(mask, [inner_lip], 255)
+    tooth_landmark = cv2.bitwise_and(tooth_landmark, tooth_landmark, mask=mask)
+    landmark += tooth_landmark
 
     cv2.imwrite(output_folder+'/{}'.format(os.path.basename(file)) , landmark)
     # cv2.imshow('source', img)
     # cv2.imshow('landmark', landmark)
-    # cv2.imshow('inner', inner_lip_masked)
-    # cv2.imshow('tooth', tooth)
-    # cv2.waitKey(10000)
+    # while(True):
+    #     if(cv2.waitKey(10) != -1):
+    #         break
+
+print('<<error file list>>')
+print(err_list)
